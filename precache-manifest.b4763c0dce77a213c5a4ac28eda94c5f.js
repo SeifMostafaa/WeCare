066f1a088bd6f71ddc5c1df60a5e94de95bcc268
SeifMostafaa <saif.mfayez@gmail.com>
2020-06-19 13:12:43 +0200

    Updates

---
+++ precache-manifest.b4763c0dce77a213c5a4ac28eda94c5f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "620f3a2311dd2271b7013a8027e9107f",
+    "url": "/WeCare/index.html"
+  },
+  {
+    "revision": "22bfb3e80a80ae60116d",
+    "url": "/WeCare/static/css/main.b09e84cb.chunk.css"
+  },
+  {
+    "revision": "f05345fa3e3d8709d349",
+    "url": "/WeCare/static/js/2.884bb6da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeCare/static/js/2.884bb6da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22bfb3e80a80ae60116d",
+    "url": "/WeCare/static/js/main.348950b5.chunk.js"
+  },
+  {
+    "revision": "0c1ceee6973adbf4161d",
+    "url": "/WeCare/static/js/runtime-main.a8569e43.js"
+  },
+  {
+    "revision": "f8ab3d0b11e73b11fa144b39860b9b00",
+    "url": "/WeCare/static/media/Clip.f8ab3d0b.png"
+  },
+  {
+    "revision": "f80974bdd3e4f355dd123791f3938774",
+    "url": "/WeCare/static/media/Clip_bg.f80974bd.png"
+  },
+  {
+    "revision": "6eb3453e4fdc53943d5e5392eb9421ff",
+    "url": "/WeCare/static/media/Clip_bk.6eb3453e.png"
+  },
+  {
+    "revision": "b92901e7897d48894ed150f07e790649",
+    "url": "/WeCare/static/media/Group_1_hv.b92901e7.png"
+  },
+  {
+    "revision": "2e4005e0fef7fc0378691c4d0a654f3d",
+    "url": "/WeCare/static/media/ID1.2e4005e0.png"
+  },
+  {
+    "revision": "f4bcc0c6cd52e84680a6e9a4d2384123",
+    "url": "/WeCare/static/media/ID10.f4bcc0c6.png"
+  },
+  {
+    "revision": "35a173780c542c3a38be96b5497124be",
+    "url": "/WeCare/static/media/ID11.35a17378.png"
+  },
+  {
+    "revision": "d3492445dca12b2e714b741e7ab0d27c",
+    "url": "/WeCare/static/media/ID12.d3492445.png"
+  },
+  {
+    "revision": "b0f9d3dd30254f801b8bdcad6d269a6d",
+    "url": "/WeCare/static/media/ID2.b0f9d3dd.png"
+  },
+  {
+    "revision": "89a16b14e632beacb8f4ec8f1ca616c9",
+    "url": "/WeCare/static/media/ID3.89a16b14.png"
+  },
+  {
+    "revision": "5ab7b782327d05660249545efa08c4ec",
+    "url": "/WeCare/static/media/ID4.5ab7b782.png"
+  },
+  {
+    "revision": "355ec3c03120646e679291d7cbfc16be",
+    "url": "/WeCare/static/media/ID5.355ec3c0.png"
+  },
+  {
+    "revision": "6f34271be28e206ee330ab5cb02d3492",
+    "url": "/WeCare/static/media/Rectangle_27.6f34271b.png"
+  }
+]);
